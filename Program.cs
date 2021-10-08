40dfa198a2bd377096beedb77d6f5364a90b806c
monir.elbaz <monir.elbaz@HP-MEB>
2021-10-08 11:08:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oef Elbaz monir ");
+            var number1 = 3;
+            var number2 = 4;
+            var uitkomst=0;
+            uitkomst = number1 + number2;
+            Console.WriteLine(uitkomst);
+        }
+    }
+}
