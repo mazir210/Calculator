d8db9c250a7a96a4e0deb304f192d93ddac7aec4
Monir El Baz <monir.elbaz@recomatics.com>
2021-10-08 11:22:38 +0200

    Exe 1

--- Program.cs
+++ Program.cs
@@ -6,12 +6,31 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Exercise1();
+            Exercise2();
+            Console.ReadLine();
+        }
+
+        public static void Exercise1()
+        {
             Console.WriteLine("Oef Elbaz monir ");
             var number1 = 3;
             var number2 = 4;
-            var uitkomst=0;
+            var uitkomst = 0;
+            uitkomst = number1 * number2;
+            Console.WriteLine(uitkomst);
+
+        }
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("Oef Elbaz monir ");
+            var number1 = 3;
+            var number2 = 4;
+            var uitkomst = 0;
             uitkomst = number1 + number2;
             Console.WriteLine(uitkomst);
+
         }
     }
 }
